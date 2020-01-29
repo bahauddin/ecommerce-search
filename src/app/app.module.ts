4665b3fcc499c1054f14c869f20ecc1613323fb2
Bahauddin <bahactg@gmail.com>
2020-01-30 01:39:45 +0600

    Header and product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftNavComponent,
+    ProductsListComponent,
+    ProductDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
